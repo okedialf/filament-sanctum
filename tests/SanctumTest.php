6562c08486098b47179ff1b555d8116d1420bd5d
Alfred Okedi <okedialf@gmail.com>
2025-01-30 08:31:57 +0300

    Updated to support laravel/sanctum ^4.0

--- tests/SanctumTest.php
+++ tests/SanctumTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class SanctumTest extends TestCase
 {
     /** @test */
-    public function it_can_getPackageDetail()
+    public function it_can_get_package_detail()
     {
         $this->assertTrue(true);
     }
